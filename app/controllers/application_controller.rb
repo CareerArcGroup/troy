8b086eb98de84da49ea248dee68a1e002bfd23f6
Artin Boghosian <aboghosian@careerarc.com>
2021-01-05 14:13:00 -0800

    Change to find_or_create_by

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def current_user
-    @current_user ||= User.create_or_find_by(name: 'Troy')
+    @current_user ||= User.find_or_create_by(name: 'Troy')
   end
 end
